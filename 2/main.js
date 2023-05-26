42f27249f63995202fb4e519000926a227ae172c
longnm2000 <longnguyenmanh20052000@gmail.com>
2023-05-26 11:23:19 +0700

    code

--- 2/main.js
+++ 2/main.js
@@ -1,15 +1,21 @@
-let inputString = " Super       HErO is     ME   ";
-let wordList = inputString.trim().toLowerCase().split(" ");
-let removedSpaceArray = [];
-for (let i = 0; i < wordList.length; i++) {
-  if (+wordList[i] !== 0) {
-    removedSpaceArray.push(wordList[i]);
+function removeSpace(arr) {
+  let wordList = arr.trim().toLowerCase().split(" ");
+  let removedSpaceArray = [];
+  for (let i = 0; i < wordList.length; i++) {
+    if (+wordList[i] !== 0) {
+      removedSpaceArray.push(wordList[i]);
+    }
   }
+  let resultArray = [];
+  for (let i = 0; i < removedSpaceArray.length; i++) {
+    resultArray.push(
+      removedSpaceArray[i].charAt(0).toUpperCase() +
+        removedSpaceArray[i].slice(1)
+    );
+  }
+  return resultArray.join(" ");
 }
-let resultArray = [];
-for (let i = 0; i < removedSpaceArray.length; i++) {
-  resultArray.push(
-    removedSpaceArray[i].charAt(0).toUpperCase() + removedSpaceArray[i].slice(1)
-  );
-}
-console.log(`"${resultArray.join(" ")}"`);
+
+console.log(`"${removeSpace("I LiKE UBUNTU VerY MuCh  ")}"`);
+console.log(`"${removeSpace(" this is A tEst ")}"`);
+console.log(`"${removeSpace(" heLlo riKkei  academy ")}"`);
