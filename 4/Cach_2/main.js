5f7de1eba762266a005f10372e3f18807682367f
longnm2000 <longnguyenmanh20052000@gmail.com>
2023-05-26 11:26:11 +0700

    code

--- 4/Cach_2/main.js
+++ 4/Cach_2/main.js
@@ -1,11 +1,14 @@
 let arr = [3, 25, 38, 49, 12];
-for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length; j++) {
-    if (arr[i] > arr[j]) {
-      let tg = arr[i];
-      arr[i] = arr[j];
-      arr[j] = tg;
+function sortDesArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let tg = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tg;
+      }
     }
   }
+  return arr;
 }
-console.log(arr);
+console.log(sortDesArray(arr));
